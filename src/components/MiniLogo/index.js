2a7301036e44ba1fc98a54bddfd20c3dd7b27043
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2021-03-31 16:04:18 -0300

    Criada a top-bar e o logo em tamanho menor

---
+++ src/components/MiniLogo/index.js
@@ -0,0 +1,14 @@
+import './mini-logo.sass'
+
+function MiniLogo () {
+  return (
+    <div className='logo'>
+      <p className='title'>
+        Github
+        <span className='sub-title'>Search</span>
+      </p>
+    </div>
+  )
+}
+
+export default MiniLogo
---
+++ src/components/TopBar/index.js
@@ -0,0 +1,17 @@
+import MiniLogo from './../MiniLogo'
+import SearchBar from './../SearchBar'
+
+import React from 'react'
+
+function TopBar () {
+  return (
+    <div className='container-top-bar'>
+      <React.Fragment>
+        <MiniLogo />
+        <SearchBar />
+      </React.Fragment>
+    </div>
+  )
+}
+
+export default TopBar
