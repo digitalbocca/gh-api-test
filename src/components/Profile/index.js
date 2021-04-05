a313057e2554de77249240da1e3f237d478758b7
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2021-04-05 11:36:14 -0300

    Implementada uma renderizaçao condicional na pagina Profile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,15 +1,29 @@
+import { useState } from 'react'
 import './profile.sass'
 import TopBar from './../TopBar'
 
-function NotFound () {
-  return (
-    <div className='container-not-found'>
-      <TopBar />
-      <div className='main-content'>
-        <p className='main-text'>User not found :(</p>
+function Profile () {
+  let [userInfo] = useState({ loading: true })
+
+  if (userInfo.loading) {
+    return (
+      <div className='container-not-found'>
+        <TopBar />
+        <div className='main-content'>
+          <p className='main-text'>Carregando...</p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className='container-not-found'>
+        <TopBar />
+        <div className='main-content'>
+          <p className='main-text'>User not found :</p>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default NotFound
+export default Profile
