d7b77ebb9d69d2e6d3bb1d13fd5ed608c0b28ead
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2021-04-05 10:39:06 -0300

    Implementada a captura do valor digitado no campo de pesquisa

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import './search-bar.sass'
 import SearchIcon from './../../assets/img/search-icon.svg'
 
 function SearchBar () {
+  let [ghSlug, setGhSlug] = useState('')
+
   return (
     <div className='search-bar'>
-      <input className='search-input' type='text' />
-      <button className='search-button'>
+      <input className='search-input' type='text' value={ghSlug} onChange={e => setGhSlug(e.target.value)} />
+      <button className='search-button' onClick={() => console.log(ghSlug)}>
         <img src={SearchIcon} alt='search' />
       </button>
     </div>
